9804e52dd66d22d08b1104e51aeb1fe167658915
May-Abo <may.abualrejal@gmail.com>
2018-01-27 13:49:05 -0500

    Adding a read me file to the project

--- src/jdbcconnection/JdbcConnectionTest.java
+++ src/jdbcconnection/JdbcConnectionTest.java
@@ -10,65 +10,73 @@ public class JdbcConnectionTest {
 
     public static void main(String[] args) throws IOException {
 
-///////////////////////////////// TESTING CONSTANTS CLASS /////////////////////////////////
-//        Connection connection = TestConstants.openConnection();
-//        TestConstants.closeConnection(connection);
-///////////////////////////////// TESTING PROPERTIES CLASS /////////////////////////////////
-//        Connection connection2 = TestProperties.openConnectionWithProperties();
-//        TestProperties.closeConnectionWithProperties(connection2);
+// TESTING CONSTANTS CLASS 
+        Connection connection = TestConstants.openConnection();
+        TestConstants.closeConnection(connection);
+        
+// TESTING PROPERTIES CLASS 
+        Connection connection2 = TestProperties.openConnectionWithProperties();
+        TestProperties.closeConnectionWithProperties(connection2);
         UserOperation up = new UserOperation();
 
-///////////////////////////////// GET USER METHOD /////////////////////////////////
-//        Users u = up.getUser("1");
-//        if (u == null) {
-//            JOptionPane.showMessageDialog(null, "User not found ");
-//        } else {
-//
-//            JOptionPane.showMessageDialog(null, "User fname " + u.getFname());
-//        }
-///////////////////////////////// ADD USER METHOD /////////////////////////////////
-//        Users u1 = new Users("", "xxx", "xxx", "xxx@gmail", "xxx");
-//        int result = up.addUser(u1);       
-//        if(result>0){
-//            System.out.println("User added");
-//        }
-//        else{
-//           System.out.println("User not added"); 
-//        }
-///////////////////////////////// UODATE INFO METHOD /////////////////////////////////
-        Users u1 = new Users("7", "Sam", "Al_Refai", "zzz@gmail", "Amman");
-        int result = up.updateUser(u1);
-        if (result > 0) {
+// GET USER METHOD 
+        Users u = up.getUser("1");
+        if (u == null) {
+            JOptionPane.showMessageDialog(null, "User not found ");
+        } else {
+
+            JOptionPane.showMessageDialog(null, "User fname " + u.getFname());
+        }
+        
+// ADD USER METHOD 
+        Users u1 = new Users("", "bbb", "bbb", "bbb@gmail", "bbb");
+        int result = up.addUser(u1);       
+        if(result>0){
+            System.out.println("User added");
+        }
+        else{
+           System.out.println("User not added"); 
+        }
+        
+// UODATE INFO METHOD
+        Users u2 = new Users("1", "ddd", "ddd", "ddd@gmail", "ddd");
+        int result2 = up.updateUser(u2);
+        if (result2 > 0) {
             System.out.println("User updated");
         } else {
             System.out.println("User not updated");
         }
-///////////////////////////////// DELETE USER METHOD /////////////////////////////////
-//        int result = up.deleteUser("1");
-//
-//        if (result > 0) {
-//            System.out.println("User deleted");
-//        } else {
-//            System.out.println("User not deleted");
-//        }
-///////////////////////////////// GET ALL USERS METHOD /////////////////////////////////
-//            Vector v = up.getAllUsers();
-//            for(int i=0; i<v.size(); i++){
-//                
-//                Users u = (Users) v.get(i);
-//                System.out.println(u.getFname()); 
-//                System.out.println(v.get(i));      
-//                               
-//            }
-///////////////////////////////// CHECK USER EMAIL METHOD /////////////////////////////////
-//            System.out.println(up.checkUserEmail("sam@gmail"));
-///////////////////////////////// GET USERS BY PROCEDURE METHOD /////////////////////////////////
-//        ArrayList a = up.getUsersPR("0", "0");
-//        System.out.println(a.size());
-//        for(int i =0; i<a.size(); i++){
-//            System.out.println(a.get(i));
-//        }
-///////////////////////////////// ADD/DEELETE/UPDATE USERS BY PROCEDURE METHOD /////////////////////////////////
+        
+// DELETE USER METHOD
+        int result3 = up.deleteUser("1");
+
+        if (result3 > 0) {
+            System.out.println("User deleted");
+        } else {
+            System.out.println("User not deleted");
+        }
+        
+// GET ALL USERS METHOD
+            Vector v = up.getAllUsers();
+            for(int i=0; i<v.size(); i++){
+                
+                Users u4 = (Users) v.get(i);
+                System.out.println(u4.getFname()); 
+                System.out.println(v.get(i));      
+                               
+            }
+            
+// CHECK USER EMAIL METHOD
+            System.out.println(up.checkUserEmail("sam@gmail"));
+            
+// GET USERS BY PROCEDURE METHOD
+        ArrayList a = up.getUsersPR("0", "0");
+        System.out.println(a.size());
+        for(int i =0; i<a.size(); i++){
+            System.out.println(a.get(i));
+        }
+        
+// ADD/DEELETE/UPDATE USERS BY PROCEDURE METHOD
 //        int result= 0 ;
 //      // insert //
 //      Users u1 = new Users("0", "May", "Ali", "May@hotmail.com", "Amman");      
